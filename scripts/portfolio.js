4c3205f82031756d9797d485804dacd16b683446
Raymond <raymond@coxarchitecture.com>
2025-09-16 10:41:48 -0400

    Fix build errors and update assets

--- scripts/portfolio.js
+++ scripts/portfolio.js
@@ -235,59 +235,46 @@ document.addEventListener('DOMContentLoaded', () => {
                 id: 'williams-lake-house',
                 name: 'Lake Norman House',
                 images: [
-                    'images/Portfolio/New Construction/Williams Lake House/Williams Lake House (1).jpg',
-                    'images/Portfolio/New Construction/Williams Lake House/Williams Lake House (2).jpg',
-                    'images/Portfolio/New Construction/Williams Lake House/Williams Lake House (3).jpg',
-                    'images/Portfolio/New Construction/Williams Lake House/Williams Lake House (4).jpg',
-                    'images/Portfolio/New Construction/Williams Lake House/Williams Lake House (5).jpg',
-                    'images/Portfolio/New Construction/Williams Lake House/Williams Lake House (6).jpg',
-                    'images/Portfolio/New Construction/Williams Lake House/Williams Lake House (7).jpg',
-                    'images/Portfolio/New Construction/Williams Lake House/Williams Lake House (8).jpg',
-                    'images/Portfolio/New Construction/Williams Lake House/Williams Lake House (9).jpg',
-                    'images/Portfolio/New Construction/Williams Lake House/Williams Lake House (10).jpg',
-                    'images/Portfolio/New Construction/Williams Lake House/Williams Lake House (11).jpg',
-                    'images/Portfolio/New Construction/Williams Lake House/Williams Lake House (12).jpg',
-                    'images/Portfolio/New Construction/Williams Lake House/Williams Lake House (13).jpg',
-                    'images/Portfolio/New Construction/Williams Lake House/Williams Lake House (14).jpg',
-                    'images/Portfolio/New Construction/Williams Lake House/Williams Lake House (15).jpg',
-                    'images/Portfolio/New Construction/Williams Lake House/Williams Lake House (16).jpg',
-                    'images/Portfolio/New Construction/Williams Lake House/Williams Lake House (17).jpg',
-                    'images/Portfolio/New Construction/Williams Lake House/Williams Lake House (18).jpg',
-                    'images/Portfolio/New Construction/Williams Lake House/Williams Lake House (19).jpg',
-                    'images/Portfolio/New Construction/Williams Lake House/Williams Lake House (20).jpg',
-                    'images/Portfolio/New Construction/Williams Lake House/Williams Lake House (21).jpg',
-                    'images/Portfolio/New Construction/Williams Lake House/Williams Lake House (22).jpg',
-                    'images/Portfolio/New Construction/Williams Lake House/Williams Lake House (23).jpg',
-                    'images/Portfolio/New Construction/Williams Lake House/Williams Lake House (24).jpg',
-                    'images/Portfolio/New Construction/Williams Lake House/Williams Lake House (25).jpg',
-                    'images/Portfolio/New Construction/Williams Lake House/Williams Lake House (26).jpg',
-                    'images/Portfolio/New Construction/Williams Lake House/Williams Lake House (27).jpg',
-                    'images/Portfolio/New Construction/Williams Lake House/Williams Lake House (28).jpg',
-                    'images/Portfolio/New Construction/Williams Lake House/Williams Lake House (29).jpg',
-                    'images/Portfolio/New Construction/Williams Lake House/Williams Lake House (30).jpg',
-                    'images/Portfolio/New Construction/Williams Lake House/Williams Lake House (31).jpg',
-                    'images/Portfolio/New Construction/Williams Lake House/Williams Lake House (32).jpg',
-                    'images/Portfolio/New Construction/Williams Lake House/Williams Lake House (33).jpg',
-                    'images/Portfolio/New Construction/Williams Lake House/Williams Lake House (34).jpg',
-                    'images/Portfolio/New Construction/Williams Lake House/Williams Lake House (35).jpg',
-                    'images/Portfolio/New Construction/Williams Lake House/Williams Lake House (36).jpg',
-                    'images/Portfolio/New Construction/Williams Lake House/Williams Lake House (37).jpg',
-                    'images/Portfolio/New Construction/Williams Lake House/Williams Lake House (38).jpg',
-                    'images/Portfolio/New Construction/Williams Lake House/Williams Lake House (39).jpg',
-                    'images/Portfolio/New Construction/Williams Lake House/Williams Lake House (40).jpg',
-                    'images/Portfolio/New Construction/Williams Lake House/Williams Lake House (41).jpg',
-                    'images/Portfolio/New Construction/Williams Lake House/Williams Lake House (42).jpg',
-                    'images/Portfolio/New Construction/Williams Lake House/Williams Lake House (43).jpg',
-                    'images/Portfolio/New Construction/Williams Lake House/Williams Lake House (44).jpg',
-                    'images/Portfolio/New Construction/Williams Lake House/Williams Lake House (45).jpg',
-                    'images/Portfolio/New Construction/Williams Lake House/Williams Lake House (46).jpg',
-                    'images/Portfolio/New Construction/Williams Lake House/Williams Lake House (47).jpg',
-                    'images/Portfolio/New Construction/Williams Lake House/Williams Lake House (48).jpg',
-                    'images/Portfolio/New Construction/Williams Lake House/Williams Lake House (49).jpg',
-                    'images/Portfolio/New Construction/Williams Lake House/Williams Lake House (50).jpg',
-                    'images/Portfolio/New Construction/Williams Lake House/Williams Lake House (51).jpg'
+                    'images/Portfolio/New Construction/Lake House/LakeHouse (1).jpg',
+                    'images/Portfolio/New Construction/Lake House/LakeHouse (9).jpg',
+                    'images/Portfolio/New Construction/Lake House/LakeHouse (18).jpg',
+                    'images/Portfolio/New Construction/Lake House/LakeHouse (19).jpg',
+                    'images/Portfolio/New Construction/Lake House/LakeHouse (20).jpg',
+                    'images/Portfolio/New Construction/Lake House/LakeHouse (21).jpg',
+                    'images/Portfolio/New Construction/Lake House/LakeHouse (22).jpg',
+                    'images/Portfolio/New Construction/Lake House/LakeHouse (23).jpg',
+                    'images/Portfolio/New Construction/Lake House/LakeHouse (24).jpg',
+                    'images/Portfolio/New Construction/Lake House/LakeHouse (25).jpg',
+                    'images/Portfolio/New Construction/Lake House/LakeHouse (26).jpg',
+                    'images/Portfolio/New Construction/Lake House/LakeHouse (27).jpg',
+                    'images/Portfolio/New Construction/Lake House/LakeHouse (28).jpg',
+                    'images/Portfolio/New Construction/Lake House/LakeHouse (29).jpg',
+                    'images/Portfolio/New Construction/Lake House/LakeHouse (30).jpg',
+                    'images/Portfolio/New Construction/Lake House/LakeHouse (31).jpg',
+                    'images/Portfolio/New Construction/Lake House/LakeHouse (32).jpg',
+                    'images/Portfolio/New Construction/Lake House/LakeHouse (33).jpg',
+                    'images/Portfolio/New Construction/Lake House/LakeHouse (34).jpg',
+                    'images/Portfolio/New Construction/Lake House/LakeHouse (35).jpg',
+                    'images/Portfolio/New Construction/Lake House/LakeHouse (36).jpg',
+                    'images/Portfolio/New Construction/Lake House/LakeHouse (37).jpg',
+                    'images/Portfolio/New Construction/Lake House/LakeHouse (38).jpg',
+                    'images/Portfolio/New Construction/Lake House/LakeHouse (39).jpg',
+                    'images/Portfolio/New Construction/Lake House/LakeHouse (40).jpg',
+                    'images/Portfolio/New Construction/Lake House/LakeHouse (41).jpg',
+                    'images/Portfolio/New Construction/Lake House/LakeHouse (42).jpg',
+                    'images/Portfolio/New Construction/Lake House/LakeHouse (43).jpg',
+                    'images/Portfolio/New Construction/Lake House/LakeHouse (44).jpg',
+                    'images/Portfolio/New Construction/Lake House/LakeHouse (45).jpg',
+                    'images/Portfolio/New Construction/Lake House/LakeHouse (46).jpg',
+                    'images/Portfolio/New Construction/Lake House/LakeHouse (47).jpg',
+                    'images/Portfolio/New Construction/Lake House/LakeHouse (48).jpg',
+                    'images/Portfolio/New Construction/Lake House/LakeHouse (49).jpg',
+                    'images/Portfolio/New Construction/Lake House/LakeHouse (50).jpg',
+                    'images/Portfolio/New Construction/Lake House/LakeHouse (51).jpg',
+                    'images/Portfolio/New Construction/Lake House/LakeHouse (52).jpg',
+                    'images/Portfolio/New Construction/Lake House/LakeHouse (53).jpg'
                 ],
-                hero_image: 'images/Portfolio/New Construction/Williams Lake House/Williams Lake House (6).jpg',
+                hero_image: 'images/Portfolio/New Construction/Lake House/LakeHouse (Hero).jpg',
                 type: 'New Construction'
             },
             {
@@ -417,6 +404,7 @@ document.addEventListener('DOMContentLoaded', () => {
                     'images/Portfolio/Renovation+Addition/Heavy Timber Pool House/IMG_0531.jpg',
                     'images/Portfolio/Renovation+Addition/Heavy Timber Pool House/IMG-0500.jpg'
                 ],
+                hero_image: 'images/Portfolio/Renovation+Addition/Heavy Timber Pool House/Heavy+Timber_01.jpg',
                 type: 'Renovation+Addition'
             },
             {
@@ -433,6 +421,7 @@ document.addEventListener('DOMContentLoaded', () => {
                     'images/Portfolio/Renovation+Addition/Lake House Renovation/DSC_3185.JPG',
                     'images/Portfolio/Renovation+Addition/Lake House Renovation/DSC_3263.jpg'
                 ],
+                hero_image: 'images/Portfolio/Renovation+Addition/Lake House Renovation/DSC_3069.JPG',
                 type: 'Renovation+Addition'
             },
             {
@@ -450,6 +439,7 @@ document.addEventListener('DOMContentLoaded', () => {
                     'images/Portfolio/Renovation+Addition/Screened Porch Additon/DSC_0052.JPG',
                     'images/Portfolio/Renovation+Addition/Screened Porch Additon/DSC_0053.JPG'
                 ],
+                hero_image: 'images/Portfolio/Renovation+Addition/Screened Porch Additon/DSC_0044.JPG',
                 type: 'Renovation+Addition'
             },
             {
@@ -463,6 +453,7 @@ document.addEventListener('DOMContentLoaded', () => {
                     'images/Portfolio/Renovation+Addition/Wine Pavlion/WP5.jpg',
                     'images/Portfolio/Renovation+Addition/Wine Pavlion/WP6.jpg'
                 ],
+                hero_image: 'images/Portfolio/Renovation+Addition/Wine Pavlion/DSC_1425.JPG',
                 type: 'Renovation+Addition'
             },
             {
@@ -473,6 +464,7 @@ document.addEventListener('DOMContentLoaded', () => {
                     'images/Portfolio/Renovation+Addition/Myers Park Renovation/Kitchen_After.jpg',
                     'images/Portfolio/Renovation+Addition/Myers Park Renovation/Rear_After.jpg'
                 ],
+                hero_image: 'images/Portfolio/Renovation+Addition/Myers Park Renovation/Front_After.jpg',
                 type: 'Renovation+Addition'
             },
             {
@@ -483,6 +475,7 @@ document.addEventListener('DOMContentLoaded', () => {
                     'images/Portfolio/Renovation+Addition/Early Classical Facade Renovation/DSC_2724.JPG',
                     'images/Portfolio/Renovation+Addition/Early Classical Facade Renovation/DSC_2725.jpg'
                 ],
+                hero_image: 'images/Portfolio/Renovation+Addition/Early Classical Facade Renovation/DSC_1370 REV.jpg',
                 type: 'Renovation+Addition'
             },
             {
@@ -491,6 +484,7 @@ document.addEventListener('DOMContentLoaded', () => {
                 images: [
                     'images/Portfolio/Renovation+Addition/Laurel Conversion/DSC_3127.jpg'
                 ],
+                hero_image: 'images/Portfolio/Renovation+Addition/Laurel Conversion/DSC_3127.jpg',
                 type: 'Renovation+Addition'
             },
             {
@@ -499,6 +493,7 @@ document.addEventListener('DOMContentLoaded', () => {
                 images: [
                     'images/Portfolio/Renovation+Addition/Montibello Backyard Getaway/DSC_1371.jpg'
                 ],
+                hero_image: 'images/Portfolio/Renovation+Addition/Montibello Backyard Getaway/DSC_1371.jpg',
                 type: 'Renovation+Addition'
             },
             {
@@ -516,6 +511,7 @@ document.addEventListener('DOMContentLoaded', () => {
                 images: [
                     'images/Portfolio/Renovation+Addition/Colonial Revival/FrontFacade.jpg'
                 ],
+                hero_image: 'images/Portfolio/Renovation+Addition/Colonial Revival/FrontFacade.jpg',
                 type: 'Renovation+Addition'
             },
             {
@@ -524,6 +520,7 @@ document.addEventListener('DOMContentLoaded', () => {
                 images: [
                     'images/Portfolio/Renovation+Addition/Cotswold Outdoor Entertainment/4116+Faulkner-1.jpg'
                 ],
+                hero_image: 'images/Portfolio/Renovation+Addition/Cotswold Outdoor Entertainment/4116+Faulkner-1.jpg',
                 type: 'Renovation+Addition'
             },
             {
@@ -532,6 +529,7 @@ document.addEventListener('DOMContentLoaded', () => {
                 images: [
                     'images/Portfolio/Renovation+Addition/Federal Makeover/DSC_0006.jpg'
                 ],
+                hero_image: 'images/Portfolio/Renovation+Addition/Federal Makeover/DSC_0006.jpg',
                 type: 'Renovation+Addition'
             }
         ];
